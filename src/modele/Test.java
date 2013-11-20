3e24706e54970fce9fa0475eaef8ad59686a8402
danydra <dany.anani@gmail.com>
2013-11-20 10:33:05 +0100

    dernier test

--- src/modele/Test.java
+++ src/modele/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bonjour test alala");
+		System.out.println("Bonjour test alala momar le connar");
 	}
 
 }
