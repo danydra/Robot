1e3d1fb624f97a20bb6002e57e527814269d9c67
danydra <dany.anani@gmail.com>
2013-11-20 10:52:54 +0100

    loic

--- src/modele/Test.java
+++ src/modele/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bonjour test alala momar le connar c'est gejntil");
+		System.out.println("Loic est une petite catin");
 	}
 
 }
